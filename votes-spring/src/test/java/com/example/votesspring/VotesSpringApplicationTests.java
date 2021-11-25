22288397063b05479a460004178d5a089d9cef2e
ParkGunWook <saiver852@gmail.com>
2021-11-26 00:56:01 +0900

    [feat] 스프링 프로젝트 생성 #3

---
+++ votes-spring/src/test/java/com/example/votesspring/VotesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.votesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
