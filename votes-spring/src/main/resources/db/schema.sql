7f49dc0e5bf9b661d3f3c280e63ae10e2adf7dbf
ParkGunWook <saiver852@gmail.com>
2021-12-12 22:34:40 +0900

    [feat] 테이블 identiy column 변경  #26 (#27)
    테이블의 identity 칼럼을 모두 테이블 명을 prefix로 가지게 수정.
    그에 맞게 sql mapper도 변경.

--- votes-spring/src/main/resources/db/schema.sql
+++ votes-spring/src/main/resources/db/schema.sql
@@ -1,78 +1,78 @@
 -- Drop All Table if Table Exist
-DROP TABLE IF EXISTS ANSWER_HISTORY;
-DROP TABLE IF EXISTS ANSWER;
-DROP TABLE IF EXISTS QUESTION;
-DROP TABLE IF EXISTS USER;
+DROP TABLE IF EXISTS answer_history;
+DROP TABLE IF EXISTS answer;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS user;
+-- 테이블 순서는 관계를 고려하여 한 번에 실행해도 에러가 발생하지 않게 정렬되었습니다.
 
--- USER Table Create SQL
-
-CREATE TABLE IF NOT EXISTS USER
+-- user Table Create SQL
+CREATE TABLE user
 (
-    `id`          BIGINT          NOT NULL    AUTO_INCREMENT COMMENT '유저 id',
-    `username`    VARCHAR(200)    NOT NULL    UNIQUE   COMMENT '유저 이름',
-    `password`    VARCHAR(200)    NOT NULL    COMMENT '암호화된 유저 비밀번호',
+    `user_id`     BIGINT          NOT NULL    AUTO_INCREMENT COMMENT '유저 id',
+    `username`    VARCHAR(200)    NOT NULL    COMMENT '유저 이름',
     `email`       VARCHAR(200)    NOT NULL    COMMENT '유저 이메일',
-    `created_at`  TIMESTAMP       NOT NULL    DEFAULT NOW() COMMENT '생성된 시간',
-    `updated_at`  TIMESTAMP       NOT NULL    DEFAULT NOW() COMMENT '업데이트된 시간',
-    `deleted_at`  TIMESTAMP       NULL        DEFAULT NULL COMMENT '삭제된 시간',
-    PRIMARY KEY (id)
+    `password`    VARCHAR(200)    NOT NULL    COMMENT '유저 비밀번호',
+    `updated_at`  DATETIME        NOT NULL    DEFAULT NOW() COMMENT '업데이트된 시간',
+    `deleted_at`  DATETIME        NULL        COMMENT '삭제된 시간',
+    `created_at`  DATETIME        NOT NULL    DEFAULT NOW() COMMENT '생성된 시간',
+    PRIMARY KEY (user_id)
 );
 
-ALTER TABLE USER COMMENT 'oauth2를 이용해서 가입한 유저의 정보를 저장합니다.';
+ALTER TABLE user COMMENT '가입한 유저의 정보를 저장합니다.';
 
--- QUESTION Table Create SQL
-CREATE TABLE IF NOT EXISTS QUESTION
+
+-- question Table Create SQL
+CREATE TABLE question
 (
-    `id`          BIGINT          NOT NULL    AUTO_INCREMENT COMMENT 'question id',
-    `user_id`     BIGINT          NOT NULL    COMMENT '질문을 만든 유저 id',
-    `question`    VARCHAR(255)    NOT NULL    COMMENT '질문 내용',
-    `is_expired`  TINYINT         NULL        DEFAULT 0 COMMENT '투표 마감 여부',
-    `expires_at`  TIMESTAMP       NOT NULL    COMMENT '투표 기한',
-    `created_at`  TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP COMMENT '생성된 시간',
-    `updated_at`  TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP COMMENT '업데이트된 시간',
-    `deleted_at`  TIMESTAMP       NULL        DEFAULT NULL COMMENT '삭제된 시간',
-    PRIMARY KEY (id)
+    `question_id`  BIGINT          NOT NULL    AUTO_INCREMENT COMMENT 'question id',
+    `user_id`      BIGINT          NOT NULL    COMMENT '질문을 만든 유저 id',
+    `question`     VARCHAR(255)    NOT NULL    COMMENT '질문 내용',
+    `expires_at`   DATETIME        NOT NULL    COMMENT '투표 기한',
+    `created_at`   DATETIME        NOT NULL    DEFAULT NOW() COMMENT '생성된 시간',
+    `deleted_at`   DATETIME        NULL        COMMENT '삭제된 시간',
+    `is_expired`   BIT             NULL        COMMENT '투표 마감 여부',
+    PRIMARY KEY (question_id)
 );
 
-ALTER TABLE QUESTION COMMENT '유저가 만든 투표(질문)이다.';
+ALTER TABLE question COMMENT '유저가 만든 투표(질문)이다.';
 
-ALTER TABLE QUESTION
-    ADD CONSTRAINT FK_QUESTION_user_id_USER_id FOREIGN KEY (user_id)
-        REFERENCES USER (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE question
+    ADD CONSTRAINT FK_question_user_id_user_user_id FOREIGN KEY (user_id)
+        REFERENCES user (user_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
--- ANSWER Table Create SQL
-CREATE TABLE IF NOT EXISTS ANSWER
+
+-- answer Table Create SQL
+CREATE TABLE answer
 (
-    `id`           BIGINT         NOT NULL    AUTO_INCREMENT COMMENT 'answer id',
+    `answer_id`    BIGINT         NOT NULL    AUTO_INCREMENT COMMENT 'answer id',
     `question_id`  BIGINT         NOT NULL    COMMENT '답변과 연관된 질문의 id',
     `answer`       VARCHAR(45)    NOT NULL    COMMENT '답변 내용',
     `count`        BIGINT         NOT NULL    DEFAULT 0 COMMENT '답변의 응답 개수',
-    PRIMARY KEY (id)
+    PRIMARY KEY (answer_id)
 );
 
-ALTER TABLE ANSWER COMMENT '유저가 만든 투표의 질문이다.';
+ALTER TABLE answer COMMENT '유저가 만든 질문의 선택지이다.';
 
-ALTER TABLE ANSWER
-    ADD CONSTRAINT FK_ANSWER_question_id_QUESTION_id FOREIGN KEY (question_id)
-        REFERENCES QUESTION (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE answer
+    ADD CONSTRAINT FK_answer_question_id_question_question_id FOREIGN KEY (question_id)
+        REFERENCES question (question_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
--- ANSWER_HISTORY Table Create SQL
-CREATE TABLE IF NOT EXISTS ANSWER_HISTORY
+
+-- answer_history Table Create SQL
+CREATE TABLE answer_history
 (
-    `id`         BIGINT    NOT NULL    AUTO_INCREMENT COMMENT 'answer history id',
-    `user_id`    BIGINT    NOT NULL    COMMENT 'user id',
-    `answer_id`  BIGINT    NOT NULL    COMMENT 'answer id',
-    PRIMARY KEY (id)
+    `answer_history_id`  BIGINT    NOT NULL    AUTO_INCREMENT COMMENT 'question history id',
+    `user_id`            BIGINT    NOT NULL    COMMENT 'user id',
+    `answer_id`          BIGINT    NOT NULL    COMMENT 'answer id',
+    PRIMARY KEY (answer_history_id)
 );
 
-ALTER TABLE ANSWER_HISTORY COMMENT '같은 유저가 같은 답변을 하는 것을 방지합니다.';
+ALTER TABLE answer_history COMMENT '같은 유저가 같은 질문에 두번 답하는 것을 방지합니다.';
 
-ALTER TABLE ANSWER_HISTORY
-    ADD CONSTRAINT FK_ANSWER_HISTORY_user_id_USER_id FOREIGN KEY (user_id)
-        REFERENCES USER (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE answer_history
+    ADD CONSTRAINT FK_answer_history_user_id_user_user_id FOREIGN KEY (user_id)
+        REFERENCES user (user_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-ALTER TABLE ANSWER_HISTORY
-    ADD CONSTRAINT FK_ANSWER_HISTORY_answer_id_ANSWER_id FOREIGN KEY (answer_id)
-        REFERENCES ANSWER (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
-
+ALTER TABLE answer_history
+    ADD CONSTRAINT FK_answer_history_answer_id_answer_answer_id FOREIGN KEY (answer_id)
+        REFERENCES answer (answer_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
