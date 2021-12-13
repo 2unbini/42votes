85107ce4d75b574f763055245b9537c3a8c3a780
Eunbin Kwon <75126613+2unbini@users.noreply.github.com>
2021-12-14 00:18:56 +0900

    [feat] Home View 데이터 바인딩 #28 (#34)
    API 활용한 Home View 데이터 바인딩

--- votes-ios/Vote/Model/AnswerModel.swift
+++ votes-ios/Vote/Model/AnswerModel.swift
@@ -7,22 +7,8 @@
 
 import Foundation
 
-class AnswerVO {
+class Answer: Codable {
     var answer: String?
     var count: Int?
     var id: Int?
 }
-
-let answerDataSet = [("libft", 20, 1),
-                     ("get next line", 10, 2),
-                     ("printf", 5, 3),
-                     ("born2beroot", 5, 4),
-                     ("pipex", 20, 5),
-                     ("so long", 10, 6),
-                     ("fdf", 5, 7),
-                     ("fractol", 10, 8),
-                     ("minitalk", 10, 9),
-                     ("push swap", 5, 10),
-                     ("philosopher", 5, 11),
-                     ("minishell", 5, 12)
-]
