f8f24abc701320aac99e8d47eb73593b4184cb65
Eunbin Kwon <kwoneunbini@gmail.com>
2021-12-20 23:47:17 +0900

    [feat] Vote View 화면 데이터 바인딩 #30
    Home->Vote, MyVote->Vote로 이동할 때 데이터 바인딩

--- votes-ios/Vote/ViewController/HomeViewController.swift
+++ votes-ios/Vote/ViewController/HomeViewController.swift
@@ -17,7 +17,52 @@ class HomeViewController: UITableViewController {
         getAllVoteList()
     }
     
-    // MARK: - Refresh
+    // 테이블뷰를 구성하는 데이터가 몇 행인지 리턴
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.allVoteList.count
+    }
+    
+    // 행 선택시, 선택한 투표 데이터 서버로부터 받아오고 VoteView로 이동
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let questionId = allVoteList[indexPath.row].id else { fatalError("nil found in questionId") }
+        getVoteData(from: questionId)
+    }
+
+    // 셀 구성
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = self.allVoteList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "voteList", for: indexPath)
+        let label = cell.viewWithTag(Tag.voteList.rawValue) as? UILabel
+        let labelText = "Q. " + (row.question ?? "")
+
+        label?.numberOfLines = 0
+        
+        // 라벨의 속성 초기화
+        label?.textColor = UIColor.black
+        label?.attributedText = NSAttributedString(string: "")
+        
+        if row.isExpired == false {
+            label?.text = labelText
+        } else if row.isExpired == true {
+            label?.textColor = UIColor.lightGray
+            label?.attributedText = labelText.strikeThrough()
+        }
+        
+        return cell
+    }
+    
+    // 각 행의 높이 지정
+    // 글자 수가 일정 개수 이상이면 연산을 통해 높이 늘리기
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let row = self.allVoteList[indexPath.row]
+        let height = CGFloat(50 + (row.question!.count / 25) * 30)
+        
+        return height
+    }
+}
+
+// MARK: Handle Refresh Control
+extension HomeViewController {
     
     func configureRefreshControl() {
         self.tableView.refreshControl = UIRefreshControl()
@@ -33,8 +78,10 @@ class HomeViewController: UITableViewController {
             self.tableView.refreshControl?.endRefreshing()
         }
     }
-    
-    // MARK: - HTTP
+}
+
+// MARK: HTTP Request Method
+extension HomeViewController {
     
     private func getAllVoteList() {
         let baseURL = "http://42votes.site/v1/questions/all"
@@ -73,52 +120,49 @@ class HomeViewController: UITableViewController {
         }.resume()
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.allVoteList.count
+    private func getVoteData(from questionId: Int) {
+        let questionId = String(questionId)
+        let baseURL = "http://42votes.site/v1/questions/"
+        let apiURL = URL(string: baseURL + questionId)!
+        
+        var request = URLRequest(url: apiURL)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
+                print("Error: HTTP request failed")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            let dateFormatter = DateFormatter()
+            
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+            decoder.dateDecodingStrategy = .formatted(dateFormatter)
+            
+            guard let decodedVoteData = try? decoder.decode(Vote.self, from: data) else {
+                print("data decode failed")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.moveToVoteView(configureWith: decodedVoteData)
+            }
+            
+        }.resume()
     }
     
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        NSLog("\(indexPath.row) selected...")
-        
+    private func moveToVoteView(configureWith vote: Vote) {
         guard let voteVC = self.storyboard?.instantiateViewController(withIdentifier: "VoteVC") as? VoteViewController else {
             return
         }
-        
-        voteVC.questionId = allVoteList[indexPath.row].id
+        voteVC.vote = vote
         self.navigationController?.pushViewController(voteVC, animated: true)
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let row = self.allVoteList[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "voteList", for: indexPath)
-        let label = cell.viewWithTag(Tag.voteList.rawValue) as? UILabel
-        let labelText = "Q. " + (row.question ?? "")
-
-        label?.numberOfLines = 0
-        
-        // Clear Attributes of label text
-        label?.textColor = UIColor.black
-        label?.attributedText = NSAttributedString(string: "")
-        
-        if row.isExpired == false {
-            label?.text = labelText
-        } else if row.isExpired == true {
-            label?.textColor = UIColor.lightGray
-            label?.attributedText = labelText.strikeThrough()
-        }
-        
-        return cell
-    }
-    
-    // 각 행의 높이 지정
-    // 글자 수가 일정 개수 이상이면 연산을 통해 높이 늘리기
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let row = self.allVoteList[indexPath.row]
-        let height = CGFloat(50 + (row.question!.count / 25) * 30)
-        
-        return height
     }
 }
 
--- votes-ios/Vote/ViewController/MyVoteViewController.swift
+++ votes-ios/Vote/ViewController/MyVoteViewController.swift
@@ -17,7 +17,61 @@ class MyVoteViewController: UITableViewController {
         getMyVoteList()
     }
     
-    // MARK: - Refresh
+    // 테이블뷰를 구성하는 데이터가 몇 행인지 리턴
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.myVoteList.count
+    }
+    
+    // 행 선택시, 선택한 투표 데이터 서버로부터 받아오고 VoteView로 이동
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let questionId = myVoteList[indexPath.row].id else { fatalError("nil found in questionId") }
+        getVoteData(from: questionId)
+    }
+
+    // 셀 구성
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = self.myVoteList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "voteList", for: indexPath)
+        let label = cell.viewWithTag(Tag.voteList.rawValue) as? UILabel
+        let labelText = "Q. " + (row.question ?? "")
+        
+        label?.numberOfLines = 0
+        
+        // 라벨의 속성 초기화
+        label?.textColor = UIColor.black
+        label?.attributedText = NSAttributedString(string: "")
+        
+        if row.isExpired == false {
+            label?.text = labelText
+        } else {
+            label?.textColor = UIColor.lightGray
+            label?.attributedText = labelText.strikeThrough()
+        }
+        
+        return cell
+    }
+    
+    // 각 행의 높이 지정
+    // 글자 수가 일정 개수 이상이면 연산을 통해 높이 늘리기
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let row = self.myVoteList[indexPath.row]
+        let height = CGFloat(50 + (row.question!.count / 25) * 30)
+        
+        return height
+    }
+    
+    // TODO: Not working
+    // 스와이프해서 삭제
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            myVoteList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
+
+// MARK: Handle Refresh Control
+extension MyVoteViewController {
     
     private func configureRefreshControl() {
         self.tableView.refreshControl = UIRefreshControl()
@@ -33,6 +87,10 @@ class MyVoteViewController: UITableViewController {
             self.tableView.refreshControl?.endRefreshing()
         }
     }
+}
+
+// MARK: HTTP Request Method
+extension MyVoteViewController {
     
     private func getMyVoteList() {
         let user = "1"
@@ -69,64 +127,50 @@ class MyVoteViewController: UITableViewController {
                 self.tableView.reloadData()
             }
         }.resume()
-        
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.myVoteList.count
+    private func getVoteData(from questionId: Int) {
+        let questionId = String(questionId)
+        let baseURL = "http://42votes.site/v1/questions/"
+        let apiURL = URL(string: baseURL + questionId)!
+        
+        var request = URLRequest(url: apiURL)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
+                print("Error: HTTP request failed")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            let dateFormatter = DateFormatter()
+            
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+            decoder.dateDecodingStrategy = .formatted(dateFormatter)
+            
+            guard let decodedVoteData = try? decoder.decode(Vote.self, from: data) else {
+                print("data decode failed")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.moveToVoteView(configureWith: decodedVoteData)
+            }
+            
+        }.resume()
     }
     
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        NSLog("\(indexPath.row) selected...")
-        
+    private func moveToVoteView(configureWith vote: Vote) {
         guard let voteVC = self.storyboard?.instantiateViewController(withIdentifier: "VoteVC") as? VoteViewController else {
             return
         }
-        
-        voteVC.questionId = myVoteList[indexPath.row].id
+        voteVC.vote = vote
         self.navigationController?.pushViewController(voteVC, animated: true)
     }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let row = self.myVoteList[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "voteList", for: indexPath)
-        let label = cell.viewWithTag(Tag.voteList.rawValue) as? UILabel
-        let labelText = "Q. " + (row.question ?? "")
-        
-        label?.numberOfLines = 0
-        
-        // Clear Attributes of label text
-        label?.textColor = UIColor.black
-        label?.attributedText = NSAttributedString(string: "")
-        
-        if row.isExpired == false {
-            label?.text = labelText
-        } else {
-            label?.textColor = UIColor.lightGray
-            label?.attributedText = labelText.strikeThrough()
-        }
-        
-        return cell
-    }
-    
-    // 각 행의 높이 지정
-    // 글자 수가 일정 개수 이상이면 연산을 통해 높이 늘리기
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let row = self.myVoteList[indexPath.row]
-        let height = CGFloat(50 + (row.question!.count / 25) * 30)
-        
-        return height
-    }
-    
-    // TODO: Not working
-    // 스와이프해서 삭제
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            myVoteList.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
 }
-
